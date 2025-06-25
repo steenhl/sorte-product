db81beb76e9a61e2184a491ce6cf173779f94461
Steen hjalmar larsen <sthl@youmail.dk>
2025-06-25 14:18:27 +0200

    search-product

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,12 @@
 import { useEffect, useState } from 'react';
 import './App.css'
-import InputElement from './components/InputElement';
-import useFetchData from './hooks/useFetchData.jsx'
+import Search from './components/Search';
+
 function App() {
-  // data
-  const { data, isPending, error } = useFetchData('http://localhost:4001/products');
-  const [products, setProducts] = useState([]);
-  // InputElement.jsx
-  const [selectedinput, setSelectedinput] = useState([]); // id'er som streng
-
-  // Når data hentes første gang
-  useEffect(() => {
-    if (data) {
-      setProducts(data);
-    }
-  }, [data]);
-
-  // Når selectedinput ændres, filtrer produkter baseret på valgte input
-  // Her antager vi, at selectedinput er en liste af brand - navne som str
-  useEffect(() => {
-    // console.log(selectedinput);
-    if (selectedinput.length > 0) {
-
-      const filteredProducts = data.filter(product =>
-        selectedinput.some(inputId => product.brand.toLowerCase() === inputId.toLowerCase())
-      );
-      setProducts(filteredProducts);
-    } else {
-      setProducts(data); // Hvis intet valgt, vis alle
-    }
-  }, [selectedinput]);
 
   return (
     <>
-      {error && <div className="error">{error}</div>}
-      {isPending && <div className="loading">Loading...</div>}
-      {products && products.map(product => <div key={product.id} className="product">${product.brand}</div>)}
-      <InputElement selectedinput={selectedinput} setSelectedinput={setSelectedinput} />
+      <Search searchType={["brand", "type", "model"]} url={"http://localhost:4001"} />
     </>
   )
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useState, useRef } from "react";
+import { IoSearch } from "react-icons/io5";
+import amplifier from "../assets/images/amplifier.png"
+import useFetchData from "../hooks/useFetchData";
+
+export default function Search({ searchType, url }) {
+    // states
+    const [originalProducts, setOriginalProducts] = useState([]);
+    const [searchResults, setSearchResults] = useState([]);
+
+    const [search, setSearch] = useState("");
+    const [errorKeyWord, setErrorKeyWord] = useState(null);
+    // refs
+    const inputRef = useRef();
+    const dialogRef = useRef();
+
+    // fetch data
+    const { data, isPending, error } = useFetchData(`${url}/products`)
+    // kun første gang komponenten renderes, så sætter vi originalProducts og searchResults
+    useEffect(() => {
+        if (data) {
+            setOriginalProducts(data);
+            setSearchResults(data);
+        }
+    }, [data]);
+
+    useEffect(() => {
+
+        if (!searchType || searchType.length === 0) {
+            setErrorKeyWord("HUSK AT ANGIVE NØGLEORD SOM PARAMETER TIL Search Componentet");
+            setSearchResults?.([]); // ingen resultater
+            return;
+        }
+
+        setErrorKeyWord(null);
+
+        const filtered = originalProducts.filter(item =>
+            searchType.some(key => {
+                const value = item[key?.toLowerCase()]?.toString().toLowerCase();
+                return value?.includes(search);
+            })
+        );
+
+        // 1. hvis søgestrengen(search) er ikke tom, så vis resultaterne
+        // 2. Hvis søgestrengen(search) er tom, så vis ingenting
+        if (search != "") {
+            setSearchResults?.(filtered);
+        }
+        else {
+            setSearchResults?.([])
+        }
+
+
+    }, [search, originalProducts]);
+
+    useEffect(() => {
+
+        if (search !== "" && searchResults.length) {
+            dialogRef.current.show()
+            inputRef.current.focus()
+        }
+        else {
+            dialogRef.current.close();
+
+        }
+
+    }, [search, searchResults])
+
+    return (
+        <div className="search-section grid grid-cols-1">
+            {isPending && <p>Loading...</p>}
+            {error && <p style={{ color: "red" }}>{error}</p>}
+
+            {errorKeyWord && <p style={{ color: "red" }}>{errorKeyWord}</p>}
+            <IoSearch />
+            <input
+                type="text"
+                id="brands"
+                name="brands"
+                placeholder="Søg produkter..."
+                onChange={(e) => setSearch(e.target.value.toLowerCase())}
+                ref={inputRef}
+
+            />
+            <dialog className="search-dialog" ref={dialogRef} >
+                <ul>
+                    {searchResults && searchResults.map((product) => {
+
+                        return <li key={product.model} >
+                            <img src={amplifier}></img>
+                            <strong>{product.brand}</strong> - {product.model}
+                        </li>
+
+                    })}
+                </ul>
+            </dialog>
+        </div>
+
+    );
+}
